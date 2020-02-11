8a93b50323ce8480e45551c67b8ffeff493d912f
Vladimir <44238165+Vladsir84@users.noreply.github.com>
2020-02-11 15:55:29 +0200

    Update index.js

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -4,13 +4,19 @@ import './styles.css';
 
 const rootElement = document.querySelector('#root');
 
-const element = ( <
-    div className = "greeting" >
-    <
-    div className = "greeting__title" > Hello, world! < /div>   <
-    div className = "greeting__text" > I 'm learning React</div>     < /
-    div >
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const rootElement = document.querySelector('#root');
+
+const element = (
+    <div className="greeting">
+        <h1 className="greeting__title">Hello, world!</h1>
+        <p className="greeting__text">I'm learning React</p>
+    </div>
 );
 
-
 ReactDOM.render(element, rootElement);
+
+
