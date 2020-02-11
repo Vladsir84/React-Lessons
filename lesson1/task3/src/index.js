3975a5951af9db6aa807334d66e655aafb99cda4
John Doe <vperedz@gmail.com>
2020-02-11 16:26:16 +0200

    add

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -4,12 +4,11 @@ import './styles.css';
 
 const rootElem = document.querySelector('#root');
 
-const container = ( <
-    div className = "greeting" >
-    <
-    h1 className = "greeting__title" > Hello, world! < /h1> <
-    p className = "greeting__text" > I 'm learning React</p> <
-    /div>
+const container = (
+    <div className="greeting">
+        <h1 className="greeting__title">Hello, world!</h1>
+        <p className="greeting__text">I'm learning React</p>
+    </div>
 );
 
 ReactDOM.render(container, rootElem);
