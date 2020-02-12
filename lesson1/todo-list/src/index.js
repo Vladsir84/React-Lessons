9531d09931c14a96d49ea8d8e2262b6043518720
John Doe <vperedz@gmail.com>
2020-02-12 11:55:49 +0200

    add

---
+++ lesson1/todo-list/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootElement = document.querySelector('#root');
+
+const element = (
+<body>
+    <div className='title'>Todo List</div>
+</body>
+);
+
+ReactDOM.render(element, rootElement);
