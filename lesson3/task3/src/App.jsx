44bd788da420c9b311e5e568be976b0a712595e1
John Doe <vperedz@gmail.com>
2020-02-13 17:53:42 +0200

    add

--- lesson3/task3/src/App.jsx
+++ lesson3/task3/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Comment from './Comment';
 
-const userInfo = {
+const author = {
     name: 'Tom',
-    avatarUrl: 'https://avatars3.githubusercontent.com/u10001'
+    avatarUrl: 'https://avatars3.githubusercontent.com/u10001',
   };
   
 const App = () => {
   return (
     <Comment 
-    name={userInfo}
+    name={author}
     text="Good job!"
     date={new Date('2019-01-01T11:32:19.566Z')}
     /> 
--- lesson3/task3/src/Comment.jsx
+++ lesson3/task3/src/Comment.jsx
@@ -3,17 +3,17 @@ import "./comment.scss";
 import moment from "moment";
 import UserInfo from './UserInfo';
 
-const formatDate = date => moment(date).format("DD MMM YYYY");
+const date = date => moment(date).format("DD MMM YYYY");
 
 function Comment(props) {
     return (
         <div className="comment">
-            <UserInfo user={props.user} />
+            <UserInfo user={props.author} />
             <div className="comment__text">
                 {props.text}
             </div>
             <div className="comment__date">
-                {formatDate(props.date)}
+                {date(props.date)}
             </div>
         </div>
     );
