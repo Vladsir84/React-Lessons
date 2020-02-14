3df26b922829b0c5063c60e0fd01d46e951b6146
John Doe <vperedz@gmail.com>
2020-02-14 14:49:49 +0200

    add

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -6,11 +6,10 @@ const getDate = date => moment(date).format("DD MMM YYYY");
 function Profile(props) {
   return (
     <>
-      <div className="greeting__name">
+      <div className="profile__name">
         {`${props.userData.firstName} ${props.userData.lastName}`}
       </div>
-      
-      <div className="greeting__birth">
+      <div className="profile__birth">
         {`Was born ${getDate(props.userData.birthDate)} in ${props.userData.birthPlace}`}
       </div>
     </>
