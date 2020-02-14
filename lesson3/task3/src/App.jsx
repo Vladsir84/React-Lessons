752d8e3911d6a2a7c4a2d4e01459216d6cfb38df
John Doe <vperedz@gmail.com>
2020-02-14 12:38:36 +0200

    add

---
+++ lesson3/task3/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Comment from './Comment';
+
+const author = {
+    name: 'Tom',
+    avatarUrl: 'https://avatars3.githubusercontent.com/u10001',
+};
+
+
+const App = () => {
+    return (
+        <Comment
+            author={author}
+            text="Good job!"
+            date={new Date('2019-01-01T11:32:19.566Z')}
+        />
+    )
+};
+
+export default App;
--- lesson3/task3/src/index.jsx
+++ lesson3/task3/src/index.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Comment from './Comment';
+import App from './App';
 
 const rootElement = document.querySelector('#root');
 
-const author = {
-    name: 'Tom',
-    avatarUrl: 'https://avatars3.githubusercontent.com/u10001',
-  };
-  
-
 ReactDOM.render(
-    <Comment 
-        author={author}
-        text="Good job!"
-        date={new Date('2019-01-01T11:32:19.566Z')}
-    />, 
+    <App />, 
       
     rootElement
 );
