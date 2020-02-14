284c979e74e879607ed21e81bb9dee842987f366
John Doe <vperedz@gmail.com>
2020-02-14 11:55:04 +0200

    add

--- lesson3/task3/src/index.jsx
+++ lesson3/task3/src/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import Comment from './Comment';
 
 const rootElement = document.querySelector('#root');
 
+const author = {
+    name: 'Tom',
+    avatarUrl: 'https://avatars3.githubusercontent.com/u10001',
+  };
+  
 
 ReactDOM.render(
-    <App
-        /> ,
+    <Comment 
+        author={author}
+        text="Good job!"
+        date={new Date('2019-01-01T11:32:19.566Z')}
+    />, 
+      
     rootElement
-    );
+);
