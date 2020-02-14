18fc8d60f3e07b04c721409f6619c49490a441df
John Doe <vperedz@gmail.com>
2020-02-14 14:29:25 +0200

    add

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -3,23 +3,15 @@ import moment from "moment";
 
 const getDate = date => moment(date).format("DD MMM YYYY");
 
-const userData = {
-  firstName: 'John',
-  lastName: 'Doe',
-  birthDate: '1991-01-17T11:11:11.819Z',
-  birthPlace: 'London'
-};
-
-
 function Profile(props) {
   return (
     <>
       <div className="profile__name">
-        {`${userData.firstName} ${userData.lastName}  `}
+        {`${props.userInfo.firstName} ${props.userInfo.lastName}`}
       </div>
       
       <div className="profile__birth">
-        {`   Was born ${getDate(userData.birthDate)} in ${userData.birthPlace}`}
+        {`Was born ${getDate(props.userInfo.birthDate)} in ${props.userInfo.birthPlace}`}
       </div>
     </>
   );
--- lesson3/task5/src/index.jsx
+++ lesson3/task5/src/index.jsx
@@ -5,9 +5,18 @@ import Profile from './Profile';
 
 const rootElement = document.querySelector('#root');
 
+const userData = {
+    firstName: 'John',
+    lastName: 'Doe',
+    birthDate: '1991-01-17T11:11:11.819Z',
+    birthPlace: 'London'
+  };
+  
+
+
 ReactDOM.render(
     <Profile
-     
+    userInfo = {userData}     
     />,
     rootElement
 );
