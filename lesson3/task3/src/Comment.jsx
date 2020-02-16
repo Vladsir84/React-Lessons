5ff37bec9bbe3169d9d0aad424be87848f1ffcfa
John Doe <vperedz@gmail.com>
2020-02-16 11:16:51 +0200

    add

--- lesson3/task3/src/Comment.jsx
+++ lesson3/task3/src/Comment.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./comment.scss";
 import moment from "moment";
-import UserInfo from './UserInfo';
+import AuthorInfo from './UserInfo';
 
 const formatDate = date => moment(date).format("DD MMM YYYY");
 
 function Comment(props) {
   return (
     <div className="comment">
-    <UserInfo author={props.author}/>
+    <AuthorInfo author={props.author}/>
       <div className="comment__text">{props.text}</div>
       <div className="comment__date">{formatDate(props.date)}</div>
     </div>
