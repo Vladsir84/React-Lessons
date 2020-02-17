e564483f7bcb7e4b035488133abbd62627a819ba
John Doe <vperedz@gmail.com>
2020-02-17 10:27:47 +0200

    add

--- lesson3/task3/src/Avatar.jsx
+++ lesson3/task3/src/Avatar.jsx
@@ -5,6 +5,7 @@ const Avatar = (props) => {
     console.log(props);   // {avatarUrl: "https://avatars3.githubusercontent.com/u10001", name: "Tom"}
                             //              avatarUrl: "https://avatars3.githubusercontent.com/u10001"
                               //            name: "Tom"
+                                     // функция принимает два параметра: аватар и имя
     return (
         <img
             className="avatar"
--- lesson3/task3/src/UserInfo.jsx
+++ lesson3/task3/src/UserInfo.jsx
@@ -3,8 +3,8 @@ import Avatar from './Avatar';
 import './user-info.scss';
 
 function UserInfo(props) {
-    console.log(props); // user: {name: "Tom", avatarUrl: "https://avatars3.githubusercontent.com/u10001" параметр, который принимает функция UserInfo
-    return (
+    console.log(props); // user: {name: "Tom", avatarUrl: "https://avatars3.githubusercontent.com/u10001" 
+    return (                          //     параметр, который принимает функция UserInfo  
         <div className="user-info">
             <Avatar
                 avatarUrl={props.user.avatarUrl}
