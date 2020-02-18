de786aa6f989831dd625d24d481effa61f451d92
John Doe <vperedz@gmail.com>
2020-02-18 12:16:49 +0200

    add

--- lesson5/task6/src/ColorPicker.jsx
+++ lesson5/task6/src/ColorPicker.jsx
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 
 class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        text: ' ',
+    };
+}
+  
   setBodyColor = color => {
-    document.body.style.backgroundColor = color;
+    this.setState({
+      text: color
+    });
   }
 
   render() {
     return (
       <div>
         <div class="picker__title">
-          Red
+        {this.state.text}
       </div>
         <div>
-          <button className="picker__button picker__button_coral"></button>
-          <button className="picker__button picker__button_aqua"></button>
-          <button className="picker__button picker__button_bisque"></button>
+          <button className="picker__button picker__button_coral"
+            onMouseOver={() => this.setBodyColor('Coral')}
+            onMouseOut={() => this.setState({
+              text: ' '
+            })}
+          />
+
+          <button className="picker__button picker__button_aqua"
+            onMouseOver={() => this.setBodyColor('Aqua')}
+            onMouseOut={() => this.setState({
+              text: ' '
+            })}
+
+          />
+
+          <button className="picker__button picker__button_bisque"
+            onMouseOver={() => this.setBodyColor('Bisque')}
+            onMouseOut={() => this.setState({
+              text: ' '
+            })}
+          />
         </div>
       </div>
     );
