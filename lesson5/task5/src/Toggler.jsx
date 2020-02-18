e801b92f811adca1aac867193084cef26b4ad41f
John Doe <vperedz@gmail.com>
2020-02-18 10:08:47 +0200

    add

---
+++ lesson5/task5/src/Toggler.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Toggler extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       text: 'Off'
+    };
+  }
+  
+    render() {
+    return (
+      <div className="toggler">
+      
+    </div> 
+     );
+   }
+}
+
+export default Toggler;
