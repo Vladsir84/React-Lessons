00787ffd4253f43befc7e5f4b30f74465780dfa4
John Doe <vperedz@gmail.com>
2020-02-19 12:44:17 +0200

    add

--- lesson6/task4/src/Status.jsx
+++ lesson6/task4/src/Status.jsx
@@ -4,14 +4,25 @@ import Offline from './Offline';
 
 
 class Status extends Component {
-  state = {
-    isOnline: true
-  };
-  
-  
-  state = {
-    Offline: false
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOnline: false,
+    };
+  }
+
+  isOnline = () => {
+    this.setState({
+      isLoggedIn: true,
+    });
+  }
+
+  Offline = () => {
+    this.setState({
+      isLoggedIn: false,
+    });
+  }
   
   render() {
   return (
