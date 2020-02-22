a091f9f6abd10480e1f021189a6769fc1bbb3a01
John Doe <vperedz@gmail.com>
2020-02-22 10:19:31 +0200

    add

--- lesson7/task2/src/UsersList.jsx
+++ lesson7/task2/src/UsersList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import User from './User'
 
-class UserList extends Component {
+class UsersList extends Component {
     state = {
         sorting: null,
     }
@@ -47,4 +47,4 @@ class UserList extends Component {
 }
 
 
-export default UserList;
+export default UsersList;
