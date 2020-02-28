b629f96d5844211d7e3a3b8191bd69f1e99bd266
John Doe <vperedz@gmail.com>
2020-02-28 13:38:25 +0200

    add

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -12,7 +12,7 @@ class Life extends Component {
     
     shouldComponentUpdate(nextProps, nextState) {
         console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-        return false 
+        return true
     }
     
     componentDidUpdate(prevProps, prevState) {
