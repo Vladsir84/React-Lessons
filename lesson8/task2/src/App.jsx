73a741d410b481dcea3c25ea4e5bb8cf62e174a3
John Doe <vperedz@gmail.com>
2020-02-28 15:53:21 +0200

    add

--- lesson8/task2/src/App.jsx
+++ lesson8/task2/src/App.jsx
@@ -25,7 +25,7 @@ class App extends Component {
                 <button onClick={this.toggle}>Toggle</button>
                 <>
                     <div>{this.state.visible && <Clock location={'New York'} offset={-5} />}</div>
-                    <div>{this.state.visible && <Clock location={'Kiev'} offset={+2} />}</div>
+                    <div>{this.state.visible && <Clock location={'Kyiv'} offset={+2} />}</div>
                     <div>{this.state.visible && <Clock location={'London'} offset={0} />}</div>
                 </>
                 
