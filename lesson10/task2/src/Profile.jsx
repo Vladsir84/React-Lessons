e3620221dd1a1f4e72be6f619fcffa274cd1041d
John Doe <vperedz@gmail.com>
2020-03-05 19:55:13 +0200

    add

---
+++ lesson10/task2/src/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import UserForm from './UserForm';
+
+const Profile = ({ userData, handleChange }) => {
+    
+    return (
+            <div className="column">
+                <UserForm userData={userData} handleChange={handleChange} />
+            </div>
+            
+        );
+    }
+
+export default Profile;
+
+
+
