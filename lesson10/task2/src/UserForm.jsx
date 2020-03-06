226bdfa163a74c3b90951c7d6ef7ab397e0ec794
John Doe <vperedz@gmail.com>
2020-03-06 13:45:15 +0200

    add

--- lesson10/task2/src/UserForm.jsx
+++ lesson10/task2/src/UserForm.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const UserForm = ({ userData, handleChange }) => {
+    
+   
     return (
+       
         <form className="user-form">
             <input
                 type="text"
