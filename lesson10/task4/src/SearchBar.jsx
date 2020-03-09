bbf41634f2e189408528a14bdcd029ec4a829651
John Doe <vperedz@gmail.com>
2020-03-09 11:54:43 +0200

    add

---
+++ lesson10/task4/src/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render() {
+      const filterText = this.props.filterText;
+      const inStockOnly = this.props.inStockOnly;
+  
+      return (
+        <form>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={filterText} />
+          <p>
+            <input
+              type="checkbox"
+              checked={inStockOnly} />
+            {' '}
+            Only show products in stock
+          </p>
+        </form>
+      );
+    }
+  }
+
+  export default SearchBar;
