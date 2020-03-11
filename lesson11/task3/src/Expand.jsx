d246df3a4b8b0e39f5de3ad96b31e83362c7c866
John Doe <vperedz@gmail.com>
2020-03-11 12:13:57 +0200

    add

--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-const Expand = ({ isOpen, children, title, onClose }) => {
-  if (!isOpen) {
-    return null;
+
+class Expand extends Component {
+  state = {
+    isOpen: false,
   }
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={onClose}>
-          <i className="fas fa-chevron-up"></i>
-        </button>
+
+  showExpand = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button className="expand__toggle-btn" onClick={this.showExpand}>
+            {this.state.isOpen ?
+              <i className="fas fa-chevron-up"></i> :
+              <i className="fas fa-chevron-down"></i>
+            }
+          </button>
+        </div>
+        <div className="expand__content">
+          {this.state.isOpen ?
+            this.props.children :
+            ''}
+        </div>
       </div>
-      <div className="expand__content">{children}</div>
-    </div>
-  );
-}
-
-Expand.propTypes = {
-  isOpen: PropTypes.bool,
-  children: PropTypes.element.isRequired,
-  title: PropTypes.string,
-  onClose: PropTypes.func.isRequired
-}
-
-Expand.defaultProps = {
-  isOpen: false,
-  title: ''
+    );
+  }
 }
 
 export default Expand;
