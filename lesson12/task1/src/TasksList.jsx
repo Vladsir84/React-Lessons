bc718c42308eb7a9321470f5ab4b95c85c2802b4
John Doe <vperedz@gmail.com>
2020-03-12 16:28:26 +0200

    add

--- lesson12/task1/src/TasksList.jsx
+++ lesson12/task1/src/TasksList.jsx
@@ -1,57 +1,52 @@
 import React, { Component } from 'react';
 import Task from './Task';
 import CreateTaskInput from './CreateTaskInput';
+import { createTask, fetchTasksList, updateTask, deleteTask } from './tasksGateway';
+
 
 class TasksList extends Component {
     state = {
-        tasks: [
-            { text: 'Buy milk', done: false, id: 1 },
-            { text: 'Pick up Tom from airport', done: false, id: 2 },
-            { text: 'Visit party', done: false, id: 3 },
-            { text: 'Visit doctor', done: true, id: 4 },
-            { text: 'Buy meat', done: true, id: 5 },
+        tasks: [],
+    };
 
-        ],
+    componentDidMount() {
+        this.fetchTasks();
+    }
+
+
+    fetchTasks = () => {
+        fetchTasksList().then(tasksList =>
+            this.setState({
+                tasks: tasksList,
+            }),
+        );
     };
 
     onCreate = text => {
-        const { tasks } = this.state;
         const newTask = {
-            id: Math.random(),
             text,
             done: false,
         };
 
-        const updatedTasks = tasks.concat(newTask);
-        this.setState({ tasks: updatedTasks });
-    }
+        createTask(newTask).then(() => this.fetchTasks());
+    };
 
     handleTaskStatusChange = id => {
-        
-        // const task = this.state.tasks.find(task => task.id);
-        // task.done = !task.done;
-        
-        const updatedTasks = this.state.tasks.map( task => {
-            if (task.id === id) {
-               return {
-                  ...task,
-                  done: !task.done 
-               } 
-            }
-        
-            return task;
-        }); 
-        this.setState({ tasks: updatedTasks });
-    }
-
-    handleTaskDelete = (id) => {
-       const updatedTasks = this.state.tasks
-       .filter(task => task.id !== id);
-       this.setState({ tasks: updatedTasks });
-    }
+        const { done, text } = this.state.tasks.find(task => task.id === id); 
+            const updatedTask = {
+                text,
+                done: !done,
+            };
+            
+        updateTask( id, updatedTask ).then(() => this.fetchTasks());    
+    };
     
-    render() {
+    handleTaskDelete = id => {
+        deleteTask(id)
+          .then(() => this.fetchTasks());
+    };
 
+    render() {
         const sortedList = this.state.tasks
             .slice()
             .sort((a, b) => a.done - b.done);
