d1c9965b22c0a8d1c8ab2d0497763c3107503f1d
John Doe <vperedz@gmail.com>
2020-04-29 12:15:55 +0300

    add

--- lesson4/task2/src/App.jsx
+++ lesson4/task2/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Clock from './Clock'
-
+import Clock from './Clock';
 
 const App = () => {
   return (
     <>
-    <Clock location={'New York'} offset={-5}/>
-    <Clock location={'Kiev'} offset={+2}/>
-    <Clock location={'London'} offset={0}/>
+      <Clock location={`London`} offset={0} />
+      <Clock location={`Kyiv`} offset={2} />
+      <Clock location={`New York`} offset={-5} />
     </>
-    );
+
+  );
 };
 
 export default App;
--- lesson4/task2/src/Clock.jsx
+++ lesson4/task2/src/Clock.jsx
@@ -2,39 +2,35 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import './clock.scss';
 
-function timeToString(offset) {
-  const getTime = new Date();
-  const timeWthOffset = getTime.setHours(getTime.getHours() + offset);
-  return moment(timeWthOffset).format('LTS');
 
+const getTimeWidthOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return moment(currentTime.setHours(currentTime.getHours() + offset + utcOffset)).format('LTS');
 }
-
 
 class Clock extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      time: timeToString(props.offset),
+      location: props.location,
+      currentTime: getTimeWidthOffset(props.offset),
     };
 
     setInterval(() => {
       this.setState({
-        time: timeToString(props.offset),
+        currentTime: getTimeWidthOffset(props.offset),
       });
     }, 1000)
   }
 
   render() {
     return (
-      <div className="clock">
-        <div className="clock__location">
-          {this.props.location}
-        </div>
-        <div className="clock__time">
-          {this.state.time}
-        </div>
+      <div className="clock" >
+        <div className="clock__location">{this.state.location}</div>
+        <div className="clock__time">{this.state.currentTime}</div>
       </div>
+
     );
   }
 }
