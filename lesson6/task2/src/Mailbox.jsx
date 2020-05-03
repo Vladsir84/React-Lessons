a2404c36cd003d57c4a2d51d6c8219ce1075871c
John Doe <vperedz@gmail.com>
2020-05-03 10:18:28 +0300

    add

--- lesson6/task2/src/Mailbox.jsx
+++ lesson6/task2/src/Mailbox.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Mailbox = ({ unreadMessages }) => {
    return (
-       <div className="mailbox">
-          <span className="mailbox__text">Messages</span>
-          {unreadMessages.length > 0  && (
+      <div className="mailbox">
+         <span className="mailbox__text">Messages</span>
+         {unreadMessages.length > 0 && (
             <span className="mailbox__count">{unreadMessages.length}</span>
-          
-          
-          )}
-           
-       </div>
-  
-    );
+
+
+         )}
+
+      </div>
+
+   );
 };
 
 export default Mailbox;
