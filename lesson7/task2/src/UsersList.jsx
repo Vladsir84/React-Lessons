0a35beaaff851896f163bd8a8e736049b2f46758
John Doe <vperedz@gmail.com>
2020-05-04 10:35:00 +0300

    add

--- lesson7/task2/src/UsersList.jsx
+++ lesson7/task2/src/UsersList.jsx
@@ -8,31 +8,31 @@ class UsersList extends Component {
 
     toggleSorting = () => {
         const newSorting = this.state.sorting === 'asc'
-        ? 'desc'
-        : 'asc';  
-        
+            ? 'desc'
+            : 'asc';
+
         this.setState({
-         sorting: newSorting, 
+            sorting: newSorting,
         });
     }
-    
-    
-    
+
+
+
     render() {
         let usersList;
-        
+
         if (this.state.sorting) {
-             usersList = this.props.users.slice().sort((a, b) => 
-             this.state.sorting === 'asc' ? a.age - b.age : b.age - a.age
+            usersList = this.props.users.slice().sort((a, b) =>
+                this.state.sorting === 'asc' ? a.age - b.age : b.age - a.age
             );
         } else {
-          usersList = this.props.users;  
+            usersList = this.props.users;
         }
-        
+
         return (
             <div>
                 <button className="btn" onClick={this.toggleSorting}>
-                    {this.state.sorting || '-'  }
+                    {this.state.sorting || '-'}
                 </button>
 
 
