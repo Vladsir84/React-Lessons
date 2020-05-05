f91179accca762152ad489a79e95bb12f6d65beb
John Doe <vperedz@gmail.com>
2020-05-05 14:31:23 +0300

    add

--- lesson8/task2/src/App.jsx
+++ lesson8/task2/src/App.jsx
@@ -1,44 +1,37 @@
 import React, { Component } from 'react';
-import moment from 'moment';
+import Clock from './Clock';
 
-
-
-const getTimeWidthOffset = offset => {
-    const currentTime = new Date();
-    const utcOffset = currentTime.getTimezoneOffset() / 60;
-    return moment(currentTime.setHours(currentTime.getHours() + offset + utcOffset)).format('LTS');
-}
-
-
-class Clock extends Component {
+class App extends Component {
     constructor(props) {
-        super(props);
+        super(props)
+
         this.state = {
-            date: getTimeWidthOffset(props.offset),
+            visible: true,
         };
+
+        this.toggle = this.toggle.bind(this);
     }
 
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            this.setState({
-                date: getTimeWidthOffset(props.offset),
-            });
-        }, 1000);
-    }
+    toggle() {
+        this.setState({
+            visible: !this.state.visible
+        });
+    };
 
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
 
     render() {
         return (
-            <div className="clock">
-                <div className="clock__location">{this.props.location}</div>
-                <div className="clock__time">{this.state.date}</div>
-            </div>
-
-        )
+            <>
+                <button onClick={this.toggle}>Toggle</button>
+                <>
+                    <div>{this.state.visible && <Clock location={"New York"} offset={-5} />}</div>
+                    <div>{this.state.visible && <Clock location={"Kyiv"} offset={2} />}</div>
+                    <div>{this.state.visible && <Clock location={"London"} offset={0} />}</div>
+                </>
+            </>
+        );
     }
+
 }
 
-export default Clock;
+export default App;
