455ad3ca060433c302c1df207032e54147b86599
John Doe <vperedz@gmail.com>
2020-05-05 14:36:03 +0300

    add

--- lesson8/task4/src/index.jsx
+++ lesson8/task4/src/index.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import User from './User';
 
-
 const rootElement = document.querySelector('#root');
-
 
 ReactDOM.render(<User userId="facebook" />, rootElement);
