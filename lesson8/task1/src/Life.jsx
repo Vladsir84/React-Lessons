61ec5b200d2b809749af79d9c54503031052a89b
John Doe <vperedz@gmail.com>
2020-05-05 11:25:42 +0300

    add

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 
+
 class Life extends Component {
-  constructor(props) {
-      super(props);
-      console.log('constructor: good place to create state');
+    constructor(props){
+        super(props)
+        console.log('constructor: good place to create state');
     }
-  
     componentDidMount() {
         console.log('componentDidMount: API calls, subscriptions');
     }
     
     shouldComponentUpdate() {
-        console.log('shoudlComponentUpdate(nextProps, nextState): decide to render or not render');
+        console.log(
+            'shoudlComponentUpdate(nextProps, nextState): decide to render or not to render',
+        );
         return true;
     }
     
@@ -19,16 +21,13 @@ class Life extends Component {
         console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
     }
     
-    
     componentWillUnmount() {
         console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
     }
-    
-    
     render() {
-       console.log('return React element to build DOM');      
-       return <div>11</div>
-  }
-}
+        console.log('return React element to build DOM');
+        return  <div className="number">Number</div>
+    }
+};
 
 export default Life;
