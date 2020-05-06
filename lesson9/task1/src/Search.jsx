9a10326de86ced2df653b5a03bc831a8fcd12db5
John Doe <vperedz@gmail.com>
2020-05-06 08:56:12 +0300

    add

--- lesson9/task1/src/Search.jsx
+++ lesson9/task1/src/Search.jsx
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 
-
 class Search extends Component {
-    state = { value: '' };
+    state = { value: '' }
 
-    handleChange = event => {
+    onInputChange = event => {
+     
         this.setState({ value: event.target.value })
     }
 
-    search = () => {
+    search = event => {
         event.preventDefault();
         alert(`Search text: ${this.state.value}`);
     }
-
+    
+    
     render() {
         return (
             <form className="search" onSubmit={this.search}>
                 <input type="text"
-                    onChange={this.handleChange}
+                    onChange={this.onInputChange}
                     value={this.state.value}
                     className="search__input"
                 />
-                <button className="search__button" type="submit">
-                    Search
-                    </button>
+                <button className="search__button" type="submit">Search</button>
             </form>
+
         );
     }
 }
