8bf64a5c312be9dca9db96b081bfe9900bd1bc54
John Doe <vperedz@gmail.com>
2020-05-10 12:21:44 +0300

    add

--- lesson10/task4/src/SearchBar.jsx
+++ lesson10/task4/src/SearchBar.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 
-const SearchBar = ({ filterText, inStockOnly, onFilterTextChange, onInStockChange}) => {
-    
-    const  handleFilterTextChange = (e) => {
-        onFilterTextChange(e.target.value);
-      }
-      
-     const handleInStockChange = (e) => {
-        onInStockChange(e.target.checked);
-      }
-      
-        return (
-          <form>
-            <input
-              type="text"
-              placeholder="Search..."
-              value={filterText}
-              onChange={handleFilterTextChange}
-            />
-            <p>
-              <input
-                type="checkbox"
-                checked={inStockOnly}
-                onChange={handleInStockChange}
-              />
-              {' '}
+const SearchBar = ({ filterText, inStockOnly, onFilterTextChange, onInStockChange }) => {
+
+  const handleFilterTextChange = (e) => {
+    onFilterTextChange(e.target.value);
+  }
+
+  const handleInStockChange = (e) => {
+    onInStockChange(e.target.checked);
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={filterText}
+        onChange={handleFilterTextChange}
+      />
+      <p>
+        <input
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={handleInStockChange}
+        />
+        {' '}
               Only show products in stock
             </p>
-          </form>
-        );
-      }
-  
+    </form>
+  );
+}
 
-  export default SearchBar;
+
+export default SearchBar;
